0ad3822fa813822d8ad6eecb2b39f998b2a82326
kebora <danielkebora@gmail.com>
2023-02-28 14:05:47 +0300

    Add row fragment

--- app/src/main/java/com/kebora/circlezero/MainActivity.java
+++ app/src/main/java/com/kebora/circlezero/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kebora.circlezero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_layout_placeholder_row,RowFragment.class,null)
+                .setReorderingAllowed(true)
+                .addToBackStack("row_test_cards")
+                .commit();
     }
 }
